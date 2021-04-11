a1992dd48c9e815dd74abe720918abc9f9726d5a
nima <54316106+nimadorostkar@users.noreply.github.com>
2021-04-12 02:28:56 +0430

    Add files via upload

---
+++ Chapter2/main.cpp
@@ -0,0 +1,17 @@
+
+//
+//  Created by nima dorostkar on 1/23/1400 AP.
+//
+
+#include <iostream>
+
+int main()
+{
+    float x;
+    std::cout << " please enter inch: \n";
+    std::cin>>x;
+    std::cout << " converted to cm: " << x*2.54 ;
+    return 0;
+}
+
+
